9d36bf390797242582da6f84215307f2de4ec61a
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-14 11:18:22 +0300

    handle_fs.c

---
+++ handle_fs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*
+*
+*/
+int handle_fs (char *form, char buf[], int *ind, va_list para)
+{
+int count = 0; 
+HANDLE arr =
+{
+{'c', hand_char} {'%', handpercent} {'s', hand_str}
+};
+
+
+}
