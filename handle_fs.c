ed5b3b533600ccfc547598f47b2cda7288175f37
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-14 14:05:00 +0300

    Update handle_fs.c

--- handle_fs.c
+++ handle_fs.c
@@ -1,16 +1,26 @@
 #include "main.h"
 /**
-*
-*
+* handle_fs -
+*@form: string
+*@buf: buffer
+*@ind: index track the buffer
+*@para: va_list name
+*Return num of printed char
 */
-int handle_fs (char *form, char buf[], int *ind, va_list para)
+
+int handle_fs (char *form, int n,char buf[], int *ind, va_list para)
 {
-int count = 0; 
+int count = 0, funcs = 'num of funcs'; 
 HANDLE arr =
 {
 {'c', hand_char} {'%', handpercent} {'s', hand_str}
 {'d', hand_int}
 };
 
+for (count = 0; count < funcs; count++)
+{
+if(arr[count].FS == form[n])
+  arr[count].F
+}
 
 }
