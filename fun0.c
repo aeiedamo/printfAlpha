0ef2fc37fb1d1745e5dbb3de0d4241b1c05485df
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-14 17:55:33 +0300

    Update fun0.c

--- fun0.c
+++ fun0.c
@@ -10,22 +10,31 @@ char ch = va_arg(para, int);
 return (write_char (ch, buf));
 }
 
+/**
+*
+*
+*/
 int hand_str (buf[], va_list para)
 {
-int length = 0, prntd_num;
+int length = 0;
 char *str = va_arg(para, char*);
 
 if (str == NULL)
 	return (-1);
-UNUSED buf
+UNUSED (buf);
 length = strlen(str);
 
 return (write (1, &str, length));
 }
 
-int hand_percent(char buf[], int *ind)
+/**
+*
+*
+*/
+int hand_percent(char buf[], va_list para)
 {
-UNUSED buf
+UNUSED (buf);
+UNUSED (para);
 char per = '%';
-write (1, &per, 1);
+return(write (1, &per, 1));
 }
