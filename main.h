ca0940bd62169fa4c3febf311199216e2a5cce9b
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-14 11:24:03 +0300

    main.h

--- main.h
+++ main.h
@@ -6,10 +6,16 @@
 #include <stdlib.h>
 
 #define BUF_LEN 1024
+
+/**
+  * struct format - is strucure to handle
+  *@FS: format specifer
+  *@F: pointer to function that print specific kind of argument
+  */
 typedef struct format
 {
-  
-  
+char FS;
+int (*F) ()
 } HANDLE;
 
 #endif
