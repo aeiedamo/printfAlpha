7070b3c9c7a4df9d18e3718988d7093f483a18a3
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-14 10:32:26 +0300

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _printf - print string to standerd out put
+*@format: string to be printed
+*@Return: num of char printed
+*/
+int _printf(const char *format, ...)
+{
+int prntd_char;
+
+
+
+
+  return (prntd_char)
+}
+
+/**
+  *
+  *
+  */
+int prnt_buf ()
+{
+if() 
+  
+  
+}
